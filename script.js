fd185a2e8ea0993bd2d47c0dded5b98894f2dcd5
Vetu Daniel <vetu.daniel@gmail.com>
2021-03-06 12:43:44 +0100

    version 1.0 added user input

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log(123)
+const prompt = require('prompt-sync')();
+const Confirm = require('prompt-confirm');
+
+
+const messages = {
+    array0: [],
+    array1: [],
+    array2:[]
+}
+
+
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num)
+  };
+  
+  const confirm = new Confirm('Do you Agree?')
+
+
+  const name = prompt("Hello and welcome to Vaniel's mixed-messages Project. To begin type in your name please. Name : ");
+  
+  console.log(`Hello there ${name}. This program will help you with some general advices.  `);
+  
+  
+  confirm.ask(function(answer) {
+    if(answer === true){
+        console.log('Great')
+    }else if(answer === false){
+        console.log('Thats OK. Goodbye!')
+    }
+  });
+ 
